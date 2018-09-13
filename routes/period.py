e79b97fa834d7835473336e6a3dae2b4dab67af4
Jackton Levy <jacktonbrigs@gmail.com>
2018-09-12 20:46:23 -0700

    removed css and js files

--- routes/period.py
+++ routes/period.py
@@ -1,8 +1,9 @@
 from routes import app
 from flask import render_template
+from .Classes import Student
 
 
 @app.route("/period")
 def period():
 
-    return render_template("period.html")
+    return render_template("period.html", value=Student.objects)
