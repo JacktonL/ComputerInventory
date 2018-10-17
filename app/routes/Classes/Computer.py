d6ca5aded9ac045ddea6af17d40d87878048c252
Jackton Levy <jacktonbrigs@gmail.com>
2018-10-16 21:17:25 -0700

    update 10/16/18 8:42

--- app/routes/Classes/Computer.py
+++ app/routes/Classes/Computer.py
@@ -1,10 +1,12 @@
 from mongoengine import *
+from .Software import Software
 
 
 class Computer(Document):
     number = StringField()
     status = StringField()
     os = StringField()
+    software = ReferenceField(Software)
 
 
 
