9c60d6d7efc1828a0033af797784655ed787c75c
Jackton Levy <jacktonbrigs@gmail.com>
2018-11-01 08:37:46 -0700

    period page commit 11/1/18

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,5 @@
 from app import app
 from .students import students, studentpage
 from .index import index
-from .period import period
-from .computers import computers
+from .period import *
+from .computers import *
--- app/routes/period.py
+++ app/routes/period.py
@@ -7,3 +7,19 @@ from .Classes import Student
 def period():
 
     return render_template("period.html", value=Student.objects)
+
+
+@app.route("/period/<num>", methods=["GET", "POST"])
+def periodpage(num):
+
+    student_list = []
+
+    for i in Student.objects:
+
+        if i.period == str(num):
+
+            student_list.append(i)
+
+            return render_template("periodpage.html", value=student_list, period=num)
+
+    return render_template("error.html")
