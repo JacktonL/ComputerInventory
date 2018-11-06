c0f2e03de71e07dc912bbe0f977f0f58a3291d5e
Jackton Levy <jacktonbriggs@gmail.com>
2018-11-06 13:27:46 -0800

    added about page

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,4 +3,5 @@ from .students import students, studentpage
 from .index import index
 from .period import *
 from .computers import *
-from.comments import *
+from .comments import *
+from .about import about
---
+++ app/routes/about.py
@@ -0,0 +1,8 @@
+from app.routes import app
+from flask import render_template
+
+
+@app.route("/about")
+def about():
+
+    return render_template("about.html")
